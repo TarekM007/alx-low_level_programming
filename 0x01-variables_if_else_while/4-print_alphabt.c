bcc77f5400ed7f2181eb564cc2724a050e5ec3cb
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-08 09:44:44 -0700

    g

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'q' && c != 'e')
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
