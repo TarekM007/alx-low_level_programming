270cd2b7521d6bd8d71867fcf377679c08f29fc6
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-08 10:43:00 -0700

    l

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	for (c = 'a'; c <='f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
