e89e92bf6d0d5f1e61d6528ee7d762516237bb05
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-08 09:56:36 -0700

    j

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
