0c59bde8813eca7b509eba0a112e112c2f875120
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-08 12:18:06 -0700

    t

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0 ; j < 9; j++)
+		{
+			for (k = 0 ; k <= 9; k++)
+		{			for (r = 0 ; r <= 9; r++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(' ');
+			putchar(k + '0');
+			putchar(r + '0');
+		if (i != 9 || j != 8 || k != 9 || r != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		else
+		{
+		putchar('\n');
+		}
+		}
+		}
+		}
+	}
+	return (0);
+}
