647e840e841bba3a032d9f6667d9b07d5276ae26
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-08 11:18:04 -0700

    w

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
+		if (i != 9)
 		putchar(',');
 		putchar(' ');
 	}
-		putchar('9 ');
+	putchar('\n');
 	return (0);
 }
