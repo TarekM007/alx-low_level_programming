2df9fda9ddc0e26c92e5ec4234692f911712ed63
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-12 15:01:13 -0700

    g

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - entry point
  * Description: Valuse use to generate times tables.
@@ -7,21 +7,35 @@
  */
 int main(void)
 {
-float totalsum;
+int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
 
-while (1)
+for (count = 0; count < 92; count++)
 {
 sum = fib1 + fib2;
-if (sum > 4000000)
-break;
-if ((sum % 2) == 0)
-totalsum += sum;
-
+printf("%lu, ", sum);
 fib1 = fib2;
 fib2 = sum;
 }
-printf("%.0f\n", totalsum);
-
+fib1half1 = fib1 / 10000000000;
+fib2half1 = fib2 / 10000000000;
+fib1half2 = fib1 / 10000000000;
+fib2half2 = fib2 / 10000000000;
+for (count = 93; count < 99; count++)
+{
+half1 += 1;
+half2 += 10000000000;
+}
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
+}
+printf("\n");
 return (0);
 }
