d76ab43f08cb733890a228d5a575639a76277322
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-12 10:34:38 -0700

    t

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - checks for the absolute value of a number
+ *
+ * @num: checks the input of the function
+ *
+ * Return: returns 1 or 0 or -1
+ */
+int _abs(int num)
+{
+
+if (num < 0)
+num = (-1) * num;
+return (num);
