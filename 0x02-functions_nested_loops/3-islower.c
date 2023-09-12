6f3179352600521c6c1d507a2b830c2d42dd7957
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-12 06:18:56 -0700

    k

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - prints 1 if lowercase , o otherwise
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+return (0);
+}
