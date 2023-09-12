2bbcad28b172bf3a247cc76f6f128c3fc62cb597
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-12 14:29:43 -0700

    o

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: Valuse use to generate times tables.
+ *
+ * Return: void
+ */
+int main(void)
+{
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+
+for (count = 0; count < 50; count++)
+{
+sum = fib1 + fib2;
+printf("%lu", sum);
+
+fib1 = fib2;
+fib2 = sum;
+
+if (count == 49)
+printf("\n");
+else
+printf(", ");
+}
+
+return (0);
