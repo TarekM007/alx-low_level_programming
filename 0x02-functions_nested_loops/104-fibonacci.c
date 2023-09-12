d1ba46116f3598c6b62de149dfe98117429b6e71
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-12 14:40:20 -0700

    e

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * print_fib - prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
- * @len: the length of with the fib sequance should print to.
+ * main - entry point
+ * Description: Valuse use to generate times tables.
  *
  * Return: void
  */
-void print_fib(int len)
-{
-	unsigned int target, i;
-
-	unsigned int former_l, former_r,;
-	unsigned int latter_l, latter_r;
-	unsigned int fib_l, fib_r;
-
-	f = 1;
-	l = 2;
-	target = len - 2;
-	for (i = 0 ; i < target ; i++)
-	{
-		fib = f + l;
-		printf("%d : ", i + 4);
-		printf("%.0Lf" , fib);
-		f = l;
-		l = fib;
-		if (i < target - 1)
-			printf("\n");
-	}
-	putchar('\n');
-}
-
-
-/**
- * main - Entry point,
- *
- *
- * Return: Always 0 (sucess)
- */
 int main(void)
 {
-	print_fib(98);
-	return (0);
-}
+float totalsum;
+unsigned long fib1 = 0, fib2 = 1, sum;
 
+while (1)
+{
+sum = fib1 + fib2;
+if (sum > 4000000)
+braek;
+if ((sum % 2) == 0)
+totalsum += sum;
+
+fib1 = fib2;
+fib2 = sum;
+}
+printf("%.0f\n", totalsum);
+
+return (0);
+}
