5222cea26b239d8709601ffb1db7edfb22e7d982
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-12 14:21:27 -0700

    j

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: Valuse use to generate times tables.
+ *
+ * Return: void
+ */
+int main(void)
+{
+int sum, num;
+
+for (num = 0; num < 1024; num++)
+{
+if ((num &3 == 0) || (num % 5 == 0))
+sum += num;
+}
+printf("%d\n", sum);
+
+return (0);
+}
