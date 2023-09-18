0c7dca2ad4d30023b06dbc423548ca039a3b05c0
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-18 07:14:30 -0700

    t

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_rev -   prints a string in reverse
+ *
+ * @s : a pointer to an int as parameter
+ *
+ * Return: returns void
+ */
+void print_rev(char *s)
+{
+s = "vomisA caasI - meht fo kcal eht raef I .sretupmoc raef ton od I";
+puts(s);
+}
