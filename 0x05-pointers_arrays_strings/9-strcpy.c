0cc6782e0d53655864453652dbca046b86cbcbbe
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-18 09:29:18 -0700

    k

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy -   copies strings
+ *
+ * @dest : a pointer to a char as parameter
+ *
+ * @src : a pointer to a char as parameter
+ *
+ * Return: returns value
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
