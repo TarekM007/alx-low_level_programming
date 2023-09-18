72f60dbeb2d09e8629805d97ba777f768463517e
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-18 11:47:50 -0700

    o

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -   generates different password
+ *
+ *
+ * Return: returns value
+ */
+int main(void)
+{
+int sum;
+char c;
+
+srand(time(NULL));
+while (sum <= 2645)
+{
+c = rand() % 128;
+sum += c;
+putchar(c);
+}
+putchar(2272 - sum);
+return (0);
+}
