c479a05e1c88a2750ace6331d6debb0f7b806011
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-21 16:21:43 -0700

    z

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,24 +9,22 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
+char *r = s;
+int i;
+char a[] = " \t\n,.!?\"(){}";
+int cap = 1;
 
-while (str[i] != '\0')
+while (*s)
 {
-if (str[i] >= 'a' && str[i] <= 'z')
+if (cap && *s >= 'a' && *s <= 'z')
+*s -= 32;
+cap = 0;
+for (i = 0; i < 12; i++)
 {
-if (str[i - 1] == 32 || str[i - 1] == 44)
-str[i] -= 32;
-if (str[i - 1] == 59 || str[i - 1] == 46)
-str[i] -= 32;
-if (str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40)
-str[i + 1] -= 32;
-if (str[i] == 41 || str[i] == 123 || str[i] == 125)
-str[i + 1] -= str[i] - 32;
-if (str[i] == 9 || str[i] == 10)
-str[i + 1] -= 32;
+if (*s == a[i])
+cap = 1;
 }
-i++;
+s++;
 }
-return (str);
+return (r);
 }
