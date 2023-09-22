9fa8cabd9d611c6b1f93407a39c466db8bd652cd
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-21 17:27:44 -0700

    x

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,16 +1,56 @@
 #include "main.h"
 
 /**
- * isprintableASCII - determines if n is ASCII or not
+ * isPrintableASCII - determines if n is ASCII or not
  * @n: int as parameter
  * Return: 1 if true, 0 if false
  */
 
-int isprintableASCII(int n)
+int isPrintableASCII(int n)
 {
 return (n >= 32 && n <= 126);
 }
+/**
+ * printHexes - print hex values
+ * @b: string as parameter
+ * @start: int as parameter
+ * @end: int as parameter
+ */
 
+void printHexes (char *b, int start, int end)
+{
+int i = 0;
+
+while (i < 10)
+{
+if (i < end)
+printf("%02x", *(b + start + i));
+else
+printf("  ");
+if (i % 2)
+printf(" ");
+i++;
+}
+}
+/**
+ * printASCII - print ASCII values
+ * @b: string as parameter
+ * @start: int as parameter
+ * @end: int as parameter
+ */
+void printASCII (char *b, int start, int end)
+{
+int ch, i = 0;
+
+while (i < end)
+{
+ch = *(b + i + start);
+if (!isPrintableASCII(ch))
+ch = 46;
+printf("%c", ch);
+i++;
+}
+}
 /**
  * print_buffer - print buffer values
  * @b: string as parameter
@@ -30,7 +70,6 @@ printHexes(b, start, end);
 printASCII(b, start, end);
 printf("\n");
 }
-}
-else
+} else
 printf("\n");
 }
