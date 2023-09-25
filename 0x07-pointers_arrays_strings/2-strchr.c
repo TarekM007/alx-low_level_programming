3d6798142cc5b618942ba2c3d868dffd64c883de
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-25 10:36:45 -0700

    e

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strchr - a function
+ * @s: char as parameter
+ *
+ * @c: char as parameter
+ *
+ * Return: returns a pointer
+ */
+char *_strchr(char *s, char c)
+{
+_strchr(s, c);
+return (s);
+}
