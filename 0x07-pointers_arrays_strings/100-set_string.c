96c48079988317ef14def3638789e8b5450eacc4
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-25 14:30:08 -0700

    b

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<string.h>
+/**
+ * set_string - a function
+ * @s: char as parameter
+ * @to: char as parameter
+ * Return: returns a void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
