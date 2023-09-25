8added20846893579088bff83a45d7a8be3da76b
TarekM007 <tarekmoustafa36@gmail.com>
2023-09-25 12:59:39 -0700

    h

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<string.h>
+/**
+ * print_chessboard - a function
+ * @a: char as parameter
+ *
+ * Return: returns a void
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+printf("\n");
+}
+}
