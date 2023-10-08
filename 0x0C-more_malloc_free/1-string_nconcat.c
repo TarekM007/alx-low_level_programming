30de4345701eb390e694e40f39578fb2780d4ee6
TarekM007 <tarekmoustafa36@gmail.com>
2023-10-08 11:19:51 -0700

    u

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,9 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+
 char *string_nconcat(char *s1, char *s2, unsigned int n){
 char *s, *snew;
 unsigned int i, j, l1, l2, l3, size1, size2;
