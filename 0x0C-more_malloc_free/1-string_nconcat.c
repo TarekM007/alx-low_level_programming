39d597ccc534cbe444183455c2e6b705ae227117
TarekM007 <tarekmoustafa36@gmail.com>
2023-10-08 12:27:20 -0700

    g

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
-
 /**
  * string_nconcat - concatenates two strings.
  * @s1: char as parameter.
@@ -14,30 +13,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-unsigned int i, j, l1, l2, l3, size;
+unsigned int i, j, l1, l2, size;
 
 l1 = strlen(s1);
 l2 = n * (sizeof(char));
-l3 = strlen(s2);
 size = (l1 + l2 + 1);
 if (s1 == NULL)
-s1 = "\0";
+s1 = "";
 if (s2 == NULL)
-s2 = "\0";
-s = malloc(size * sizeof(char));
+s2 = "";
+s = malloc(size *sizeof(char));
 if (s == 0)
 return (0);
-if (n >= l3)
-{
 for (i = 0; i < l1; i++)
 s[i] = s1[i];
-for (i = 0, j = l1; j < l1 + l2; j++, i++)
-s[j] = s2[i];
-s[j] = '\0';
-}
-for (i = 0; i < l1; i++)
-s[i] = s1[i];
-for (i = 0, j = l1; j < l1 + n; j++, i++)
+for (i = 0, j = l1; s[i] != '\0' && i < n; j++, i++)
 s[j] = s2[i];
 s[j] = '\0';
 return (s);
