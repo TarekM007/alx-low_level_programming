60948e8682dd796e81a1203b940de120681b526c
TarekM007 <tarekmoustafa36@gmail.com>
2023-10-08 05:40:22 -0700

    f

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,11 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-unsigned int i, j, l1, l2, size;
+unsigned int i, j, l1, l2, l3, size;
 
 l1 = strlen(s1);
 l2 = n * (sizeof(char));
+l3 = strlen(s2);
 size = (l1 + l2 + 1);
 if (s1 == NULL)
 s1 = "\0";
@@ -25,7 +26,7 @@ s2 = "\0";
 s = calloc(size, sizeof(char));
 if (s == 0)
 return (0);
-if (n >= l2)
+if (n >= l3)
 {
 for (i = 0; i < l1; i++)
 s[i] = s1[i];
