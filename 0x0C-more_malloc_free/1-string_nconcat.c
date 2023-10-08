7853dbe639a14e0dbe8baa07b15aecb77c56fd1c
TarekM007 <tarekmoustafa36@gmail.com>
2023-10-08 11:25:05 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,34 +4,37 @@
 #include <limits.h>
 #include <string.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n){
-char *s, *snew;
-unsigned int i, j, l1, l2, l3, size1, size2;
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: char as parameter.
+ * @s2: char as parameter.
+ * @n: int as parameter.
+ * Return: returns pointer.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *s;
+unsigned int i, j, l1, l2, l3, size;
 
 l1 = strlen(s1);
 l2 = n * (sizeof(char));
 l3 = strlen(s2);
-size1 = (l1 + l2 + 1);
-size2 = (l1 + l3 + 1);
+size = (l1 + l2 + 1);
 if (s1 == NULL)
 s1 = "\0";
 if (s2 == NULL)
 s2 = "\0";
-if (n >= l3)
-{
-snew = calloc(size2, sizeof(char));
-if (snew == 0)
-return (0);
-for (i = 0; i < l1; i++)
-snew[i] = s1[i];
-for (i = 0, j = l1; j < l1 + l3; j++, i++)
-snew[j] = s2[i];
-snew[j] = '\0';
-return (snew);
-}
-s = calloc(size1, sizeof(char));
+s = calloc(size, sizeof(char));
 if (s == 0)
 return (0);
+if (n >= l3)
+{
+for (i = 0; i < l1; i++)
+s[i] = s1[i];
+for (i = 0, j = l1; j < l1 + l2; j++, i++)
+s[j] = s2[i];
+s[j] = '\0';
+}
 for (i = 0; i < l1; i++)
 s[i] = s1[i];
 for (i = 0, j = l1; j < l1 + n; j++, i++)
