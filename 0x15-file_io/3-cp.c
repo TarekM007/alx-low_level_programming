a1448d116491369ca3b22dbcfb898edda709c04a
TarekM007 <tarekmoustafa36@gmail.com>
2023-11-03 19:45:07 -0700

    fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,26 +11,18 @@
  */
 int main(int argc, char *argv[])
 {
-const char *file_from = argv[1];
-const char *file_to = argv[2];
+const char *file_from = argv[1], *file_to = argv[2];
 char buffer[BUFFER_SIZE];
-int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 ssize_t bytes_read, bytes_written;
 int fd_from = open(file_from, O_RDONLY);
 
 if (argc != 3)
-{
-dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-return (97); }
+dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
 if (fd_from == -1)
-{
-dprintf(2, "Error: Can't read from file %s\n", file_from);
-return (98); }
+dprintf(2, "Error: Can't read from file %s\n", file_from), exit(98);
 if (fd_to == -1)
-{
-dprintf(2, "Error: Can't write to %s\n", file_to);
-close(fd_from);
-return (99); }
+dprintf(2, "Error: Can't write to %s\n", file_to), close(fd_from), exit(99);
 while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
@@ -39,20 +31,18 @@ if (bytes_written == -1)
 dprintf(2, "Error: Can't write to %s\n", file_to);
 close(fd_from);
 close(fd_to);
-return (99); }}
+exit(99); }}
 if (bytes_read == -1)
 {
 dprintf(2, "Error: Can't read from file %s\n", file_from);
 close(fd_from);
 close(fd_to);
-return (98); }
+exit(98); }
 if (close(fd_from) == -1)
 {
 dprintf(2, "Error: Can't close fd %d\n", fd_from);
 close(fd_to);
-return (100); }
+exit(100); }
 if (close(fd_to) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd_to);
-return (100); }
+dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
 return (0); }
