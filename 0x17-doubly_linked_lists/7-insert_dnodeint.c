adc22ec6ce26a89d693ef1752ddc2e20e36be658
TarekM007 <tarekmoustafa36@gmail.com>
2023-12-08 09:32:38 -0800

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,28 +9,38 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *ptr = *h;
-dlistint_t *ptr2 = NULL;
-dlistint_t *temp = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node, *tmpcount = *h, *tmp1 = *h, *tmp2;
+	unsigned int count = 0;
 
-if (h == NULL)
-{
-return (NULL);
-}
-temp->prev = NULL;
-temp->n = n;
-temp->next = NULL;
+	if (!h)
+		return (NULL);
+	while (tmpcount)
+		tmpcount = tmpcount->next, count++;
+	if (idx > count)
+		return (NULL);
 
-idx--;
-while (idx != 0)
-{
-ptr = ptr->next;
-idx--;
-}
-ptr2 = ptr->next;
-ptr->next = temp;
-ptr2->prev = temp;
-temp->next = ptr2;
-temp->prev = ptr;
-return (temp);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->prev = NULL, new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	for (count = 0; count < idx; count++)
+		tmp2 = tmp1, tmp1 = tmp1->next;
+
+	new_node->next = tmp1;
+	new_node->prev = tmp2;
+	tmp2->next = new_node;
+	if (tmp1)
+		tmp1->prev = new_node;
+
+	return (new_node);
 }
